eb7d70668cbaa16d69e0e3019e027a70d1b75a0e
Baptiste Doucerain <baptiste.doucerain.ext@parrot.com>
2018-10-26 19:28:40 +0200

    Build a css goban

--- src/app.js
+++ src/app.js
@@ -3,4 +3,107 @@ import ReactDOM from "react-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>THIS IS MY BOILERPLATE</p>, document.getElementById("root"));
+// const Row = (
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+//   <div className="middle" id="item_1" />
+// );
+
+const Intersection = () => {
+  return (
+    <div>
+      {/* <div className="middle" /> */}
+      <div className="container">
+        <div className="top-left" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top" id="item_1" />
+        <div className="top-right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="left" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="middle" id="item_1" />
+        <div className="right" id="item_1" />
+        <div className="bot-left" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bottom" id="item_1" />
+        <div className="bot-right" id="item_1" />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Intersection />, document.getElementById("root"));
