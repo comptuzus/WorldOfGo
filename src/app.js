df5adb98ce5116035de9469ca678650fe76d5c86
Baptiste Doucerain <baptiste.doucerain.ext@parrot.com>
2018-10-31 17:47:13 +0100

    Remove unecessary files

--- src/app.js
+++ src/app.js
@@ -1,99 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
+import Goban from "./components/Goban";
+import Areas from "./components/Areas"
+import Stones from "./components/Stones"
 
+const Game = () => (
+  <div>
+    <Goban />
+    <Areas />
+    <Stones />
+  </div>
+)
 
-
-const Intersection = () => {
-  return (
-    <div>
-      {/* <div className="middle" /> */}
-      <div className="container">
-        <div className="top-left" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top" id="item_1" />
-        <div className="top-right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="left" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="middle" id="item_1" />
-        <div className="right" id="item_1" />
-        <div className="bot-left" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bottom" id="item_1" />
-        <div className="bot-right" id="item_1" />
-      </div>
-    </div>
-  )
-}
-
-ReactDOM.render(<Intersection />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
--- src/components/Areas.js
+++ src/components/Areas.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const squareSide = 35;
 const circleRadius = 12;
-let areas = [...Array(81).keys()].map((i) => {
+
+const areas = [...Array(81).keys()].map((i) => {
   let x = parseInt(i / 9) * squareSide + 10;
   let y = i % 9 * squareSide + 10;
   return { x, y, selected: false };
